6b5551e76adfd42917005687b07b416b75e4d364
Mr.Hope <zhangbowang1998@gmail.com>
2022-01-25 14:15:55 +0800

    chore: update config files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 const { dest, parallel, src, watch } = require("gulp");
 const rename = require("gulp-rename");
 const { sass } = require("@mr-hope/gulp-sass");
-const PluginError = require("plugin-error");
 const typescript = require("gulp-typescript");
-const { Transform } = require("stream");
 const sourcemaps = require("gulp-sourcemaps");
 
 const appTSProject = typescript.createProject("tsconfig.app.json");
@@ -13,9 +11,9 @@ const buildWXSS = () =>
   src("app/**/*.scss")
     .pipe(
       sass({
-        outputStyle: "compressed",
+        style: "compressed",
         importers: [
-          // use `import:` as hack for remaining '@import'
+          // preserve `@import` rules
           {
             canonicalize: (url, { fromImport }) =>
               fromImport
@@ -32,37 +30,6 @@ const buildWXSS = () =>
       }).on("error", sass.logError)
     )
     .pipe(rename({ extname: ".wxss" }))
-    .pipe(
-      new Transform({
-        objectMode: true,
-        transform(chunk, _enc, callback) {
-          if (chunk.isNull()) {
-            this.push(chunk);
-
-            return callback();
-          }
-
-          if (chunk.isStream()) {
-            this.emit(
-              "error",
-              new PluginError("Sass", "Streaming not supported")
-            );
-
-            return callback();
-          }
-
-          const content = chunk.contents
-            .toString()
-            .replace(/@import ?"!(.*?)\.css"/gu, '@import "$1.wxss"');
-
-          chunk.contents = Buffer.from(content);
-
-          this.push(chunk);
-
-          callback();
-        },
-      })
-    )
     .pipe(dest("dist/app"));
 
 const moveAppFiles = () =>
